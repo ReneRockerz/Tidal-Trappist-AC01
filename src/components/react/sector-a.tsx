63e3bc71304e9d8fbad4a67c10be3491c6757234
ReneRockerz <63672875+ReneRockerz@users.noreply.github.com>
2024-04-25 07:49:25 +0700

    update : 4/25/2024

---
+++ src/components/react/sector-a.tsx
@@ -0,0 +1,72 @@
+"use client"
+ 
+import * as React from "react"
+ 
+import {
+    Breadcrumb,
+    BreadcrumbEllipsis,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+  } from "@/components/ui/breadcrumb"
+  import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+  
+  interface BreadcrumbProps {
+    items: {
+      label: string;
+      href?: string;
+      menu?: {
+        label: string;
+        href?: string;
+      }[];
+    }[];
+  }
+  
+  export function BreadcrumbDemo({ items }: BreadcrumbProps) {
+    return (
+      <Breadcrumb>
+        <BreadcrumbList>
+          {items.map((item, index) => (
+            <React.Fragment key={index}>
+              <BreadcrumbItem>
+                {item.href ? (
+                  <BreadcrumbLink href={item.href}>{item.label}</BreadcrumbLink>
+                ) : item.menu ? (
+                  <DropdownMenu>
+                    <DropdownMenuTrigger className="flex items-center gap-1">
+                      <BreadcrumbEllipsis className="h-4 w-4" />
+                      <span className="sr-only">Toggle menu</span>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="start">
+                      {item.menu.map((menuItem, menuIndex) => (
+                        <DropdownMenuItem key={menuIndex}>
+                          {menuItem.href ? (
+                            <BreadcrumbLink href={menuItem.href}>
+                              {menuItem.label}
+                            </BreadcrumbLink>
+                          ) : (
+                            menuItem.label
+                          )}
+                        </DropdownMenuItem>
+                      ))}
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                ) : (
+                  <BreadcrumbPage>{item.label}</BreadcrumbPage>
+                )}
+              </BreadcrumbItem>
+              {index < items.length - 1 && <BreadcrumbSeparator />}
+            </React.Fragment>
+          ))}
+        </BreadcrumbList>
+      </Breadcrumb>
+    );
+  }
+  
